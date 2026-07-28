51d2358b7c3d0ed85ed4b46d02ca819da39d1c59
Julian <julian_v_i@hotmail.com>
2026-07-28 15:43:27 -0700

    token

--- whatsappservice.py
+++ whatsappservice.py
@@ -3,7 +3,7 @@ import json
 
 def SendMessageWhatsapp(data):
     try:
-        token = "EAAeX9malBk0BSAG15puYj2ZBvdxMQFZCy2Nf1bachVkkAzi0zOQBGuGF4VInbsZBloZCkKWp1fbFqL4rPkavaPFo5sP0BDEoPAZALlkcf0Ne6QlwomNXXWhktQWAz4RZA3xS1IqGDf6igNVFwlM4BWIQfHZCuSDjK6dVdqeZATdZA9Gujjz5MK6YVgOwUBXVWpvkTMW01aNRS83owlkVzw1bMSDzy9JZB2OXZBAcmonw3dlBkvSZClKqvG93akkFfDpIRXZA67W65qSArW0bwWQPWvgoneofrUAZDZD"
+        token = "EAAeX9malBk0BSOZAhZCQ8jQ7oW5uNgbYnQZBdNmJgkR93x9WxFqwxHIkZA6avj9Nn7xJ28ZBRW7FPysrIQRbtO4aPWRWHZB1gvzZAd3caD1NL40vQP0qT6PoibFPugztgSV3J5Jc8uMfQnu1BTT6Uo6f6HleZCGz6JS6S2pUDWZCZBImDR9JgZC8YbGL0F2pN0iuUhhEjCOa8bsZBtQ0DTZCmyWxbgzpbz7KKOD4hUtgmsxkfWOna0DqINZAP6822eP7PhNA0Mw4yUZBwdjSybBg6W1rFNLR6LV"
         api_url = "https://graph.facebook.com/v25.0/1135356866337944/messages"
         headers = {"Content-Type": "application/json", "Authorization": "Bearer " + token}
         response = requests.post(api_url, data = json.dumps(data), headers=headers)
