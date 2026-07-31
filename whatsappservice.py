dae6f2131bb112817f1bbbc8323147273a42fec3
Julian <julian_v_i@hotmail.com>
2026-07-31 12:17:43 -0700

    api_url

--- whatsappservice.py
+++ whatsappservice.py
@@ -4,7 +4,7 @@ import json
 def SendMessageWhatsapp(data):
     try:
         token = "EAAeX9malBk0BSCQmFmA0ycSZBLZCSPvobOXvXZBJutdQgoyfFcSG5xefrrQ9BvdPEAh2z0ABeNRP2dPdQK8NVRmqOzgfTSLyMKx0eBFESZA1s0WCteKmr0Gkgz9slM5y3hW6ZBKG7yehRtO95AOeHkW1G0tDNYMWLWwcyJl0hmky2ZCJz2GDTpnvX2i0nBeyyWFwZDZD"
-        api_url = "https://graph.facebook.com/v25.0/1135356866337944/messages"
+        api_url = "https://graph.facebook.com/v25.0/1301991842988706/messages"
         headers = {"Content-Type": "application/json", "Authorization": "Bearer " + token}
         response = requests.post(api_url, data = json.dumps(data), headers=headers)
         if response.status_code == 200:
